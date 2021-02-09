8585fdd52bb626c324b94804abfe0387041ed93b
Mahmoud Hamdy <42287052+MahmoudHamdy00@users.noreply.github.com>
2021-02-09 08:50:12 +0200

    Add project files.

---
+++ Mail-Sender-App/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Mail_Sender_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.senderEmail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Subject = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Body = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Emails = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.senderEmail);
+            this.groupBox1.Location = new System.Drawing.Point(21, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Credentials";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Email";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(95, 72);
+            this.password.Name = "password";
+            this.password.PasswordChar = '•';
+            this.password.Size = new System.Drawing.Size(100, 22);
+            this.password.TabIndex = 3;
+            // 
+            // senderEmail
+            // 
+            this.senderEmail.Location = new System.Drawing.Point(95, 32);
+            this.senderEmail.Name = "senderEmail";
+            this.senderEmail.Size = new System.Drawing.Size(100, 22);
+            this.senderEmail.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(493, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(342, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Subject";
+            // 
+            // Subject
+            // 
+            this.Subject.Location = new System.Drawing.Point(424, 89);
+            this.Subject.Name = "Subject";
+            this.Subject.Size = new System.Drawing.Size(206, 22);
+            this.Subject.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(342, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Body of the message";
+            // 
+            // Body
+            // 
+            this.Body.Location = new System.Drawing.Point(345, 158);
+            this.Body.Multiline = true;
+            this.Body.Name = "Body";
+            this.Body.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Body.Size = new System.Drawing.Size(277, 241);
+            this.Body.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Mail Sender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Designed by: Ma7moud.7amdy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Recipients";
+            // 
+            // Emails
+            // 
+            this.Emails.Location = new System.Drawing.Point(21, 199);
+            this.Emails.Multiline = true;
+            this.Emails.Name = "Emails";
+            this.Emails.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Emails.Size = new System.Drawing.Size(277, 241);
+            this.Emails.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Subject);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Body);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Emails);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox senderEmail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Subject;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Body;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Emails;
+    }
+}
+
---
+++ Mail-Sender-App/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+namespace Mail_Sender_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button1.Enabled = false;
+                string[] emails = Emails.Text.Split(',');
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress(senderEmail.Text);
+
+                msg.Subject = Subject.Text;
+                msg.Body = Body.Text;
+                // msg.Attachments
+                SmtpClient smtp = new SmtpClient
+                {
+                    UseDefaultCredentials = false,
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                    Port = 587,
+                    Credentials = new NetworkCredential(senderEmail.Text, password.Text)
+                };
+                foreach (string s in emails)
+                {
+                    //TO-DO: optimize it to ignore invalid mails
+                    msg.To.Clear();
+                    msg.To.Add(s);
+                    smtp.Send(msg);
+                }
+                MessageBox.Show("Mission Acomplished", "Result", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                button1.Enabled = true;
+            }
+        }
+    }
+}
