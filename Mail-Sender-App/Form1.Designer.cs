a5ec65bb357d1a0e90959dcd833c6448ee3fb70e
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2021-05-30 23:23:39 +0200

    Added source data sheet

--- Mail-Sender-App/Form1.Designer.cs
+++ Mail-Sender-App/Form1.Designer.cs
@@ -29,21 +29,37 @@ namespace Mail_Sender_App
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.password = new System.Windows.Forms.TextBox();
             this.senderEmail = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.send_btn = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.Subject = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.Body = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.Emails = new System.Windows.Forms.TextBox();
+            this.openSourceSheetbtn = new System.Windows.Forms.Button();
+            this.Subject = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.saveButton = new System.Windows.Forms.Button();
+            this.stopSendingBtn = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -52,146 +68,171 @@ namespace Mail_Sender_App
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.password);
             this.groupBox1.Controls.Add(this.senderEmail);
-            this.groupBox1.Location = new System.Drawing.Point(21, 57);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(277, 100);
-            this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Credentials";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 72);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Password";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 32);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Email";
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(95, 72);
+            resources.ApplyResources(this.password, "password");
             this.password.Name = "password";
-            this.password.PasswordChar = '•';
-            this.password.Size = new System.Drawing.Size(100, 22);
-            this.password.TabIndex = 3;
             // 
             // senderEmail
             // 
-            this.senderEmail.Location = new System.Drawing.Point(95, 32);
+            resources.ApplyResources(this.senderEmail, "senderEmail");
             this.senderEmail.Name = "senderEmail";
-            this.senderEmail.Size = new System.Drawing.Size(100, 22);
-            this.senderEmail.TabIndex = 0;
             // 
-            // button1
+            // send_btn
             // 
-            this.button1.Location = new System.Drawing.Point(493, 417);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 23);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "Send";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            resources.ApplyResources(this.send_btn, "send_btn");
+            this.send_btn.Name = "send_btn";
+            this.send_btn.UseVisualStyleBackColor = true;
+            this.send_btn.Click += new System.EventHandler(this.send_btn_Click);
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(342, 92);
+            resources.ApplyResources(this.label7, "label7");
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(55, 17);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Subject";
-            // 
-            // Subject
-            // 
-            this.Subject.Location = new System.Drawing.Point(424, 89);
-            this.Subject.Name = "Subject";
-            this.Subject.Size = new System.Drawing.Size(206, 22);
-            this.Subject.TabIndex = 15;
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(342, 129);
+            resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(141, 17);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "Body of the message";
             // 
             // Body
             // 
-            this.Body.Location = new System.Drawing.Point(345, 158);
-            this.Body.Multiline = true;
+            resources.ApplyResources(this.Body, "Body");
             this.Body.Name = "Body";
-            this.Body.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Body.Size = new System.Drawing.Size(277, 241);
-            this.Body.TabIndex = 13;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(18, 11);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 17);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Mail Sender";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(419, 11);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(203, 17);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Designed by: Ma7moud.7amdy";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 176);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 17);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Recipients";
             // 
             // Emails
             // 
-            this.Emails.Location = new System.Drawing.Point(21, 199);
-            this.Emails.Multiline = true;
+            resources.ApplyResources(this.Emails, "Emails");
             this.Emails.Name = "Emails";
-            this.Emails.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Emails.Size = new System.Drawing.Size(277, 241);
-            this.Emails.TabIndex = 9;
+            // 
+            // openSourceSheetbtn
+            // 
+            resources.ApplyResources(this.openSourceSheetbtn, "openSourceSheetbtn");
+            this.openSourceSheetbtn.Name = "openSourceSheetbtn";
+            this.toolTip1.SetToolTip(this.openSourceSheetbtn, resources.GetString("openSourceSheetbtn.ToolTip"));
+            this.openSourceSheetbtn.UseVisualStyleBackColor = true;
+            this.openSourceSheetbtn.Click += new System.EventHandler(this.openSourceSheetbtn_Click);
+            // 
+            // Subject
+            // 
+            resources.ApplyResources(this.Subject, "Subject");
+            this.Subject.Name = "Subject";
+            // 
+            // dataGridView1
+            // 
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.DataSourceChanged += new System.EventHandler(this.dataGridView1_DataSourceChanged);
+            this.dataGridView1.SizeChanged += new System.EventHandler(this.dataGridView1_SizeChanged);
+            // 
+            // tabControl1
+            // 
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.name);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.Emails);
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            this.toolTip1.SetToolTip(this.tabPage1, resources.GetString("tabPage1.ToolTip"));
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            this.toolTip1.SetToolTip(this.label8, resources.GetString("label8.ToolTip"));
+            // 
+            // name
+            // 
+            resources.ApplyResources(this.name, "name");
+            this.name.Name = "name";
+            this.toolTip1.SetToolTip(this.name, resources.GetString("name.ToolTip"));
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.stopSendingBtn);
+            this.tabPage2.Controls.Add(this.saveButton);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.openSourceSheetbtn);
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.toolTip1.SetToolTip(this.tabPage2, resources.GetString("tabPage2.ToolTip"));
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            resources.ApplyResources(this.saveButton, "saveButton");
+            this.saveButton.Name = "saveButton";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // stopSendingBtn
+            // 
+            resources.ApplyResources(this.stopSendingBtn, "stopSendingBtn");
+            this.stopSendingBtn.Name = "stopSendingBtn";
+            this.stopSendingBtn.UseVisualStyleBackColor = true;
+            this.stopSendingBtn.Click += new System.EventHandler(this.stopSendingBtn_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(649, 450);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.Subject);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.Body);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.send_btn);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.Emails);
-            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,15 +245,25 @@ namespace Mail_Sender_App
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.TextBox senderEmail;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button send_btn;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox Subject;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox Body;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox Emails;
+        private System.Windows.Forms.Button openSourceSheetbtn;
+        private System.Windows.Forms.TextBox Subject;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button stopSendingBtn;
     }
 }
 
--- Mail-Sender-App/Form1.cs
+++ Mail-Sender-App/Form1.cs
@@ -2,43 +2,70 @@
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Mail;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Threading;
+
 namespace Mail_Sender_App
 {
     public partial class Form1 : Form
     {
+        private string filePath;
+        MailMessage msg;
+        SmtpClient smtp;
+
+        Thread thread;
         public Form1()
         {
             InitializeComponent();
-        }
+           
+            filePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;// full path untill bin folder
+            filePath = Directory.GetParent(filePath).FullName;// full path untill broject folder
+            filePath = Path.Combine(filePath, "sourceDataSheet.xlsx");
 
-        private void button1_Click(object sender, EventArgs e)
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+            dataGridView1.Visible = true;
+            dataGridView1.DataSource = ReadExcel(filePath, "xlsx");
+
+            thread = new Thread(sendByIndividual);
+
+        }
+        private void send_btn_Click(object sender, EventArgs e)
         {
             try
             {
-                button1.Enabled = false;
-                string[] emails = Emails.Text.Split(',');
-                MailMessage msg = new MailMessage();
+                send_btn.Enabled = false;
+                /*MailMessage*/
+                msg = new MailMessage();
                 msg.From = new MailAddress(senderEmail.Text);
 
                 msg.Subject = Subject.Text;
-                msg.Body = Body.Text;
                 // msg.Attachments
-                SmtpClient smtp = new SmtpClient
+                /*SmtpClient*/
+                smtp = new SmtpClient
                 {
                     UseDefaultCredentials = false,
                     Host = "smtp.gmail.com",
                     EnableSsl = true,
                     Port = 587,
-                    Credentials = new NetworkCredential(senderEmail.Text, password.Text)
+                    //  DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential(senderEmail.Text, password.Text),
+                    // Timeout = 20000
+
                 };
-                foreach (string s in emails)
+                if (tabControl1.SelectedIndex == 0)
                 {
-                    //TO-DO: optimize it to ignore invalid mails
-                    msg.To.Clear();
-                    msg.To.Add(s);
-                    smtp.Send(msg);
+                    sendByGroup(msg, smtp);
                 }
-                MessageBox.Show("Mission Acomplished", "Result", MessageBoxButtons.OK);
+                else
+                {
+                    //thread.Start();
+                    //while (thread.IsAlive) ;
+                      sendByIndividual(/*msg, smtp*/);
+                }
+
             }
             catch (Exception ex)
             {
@@ -46,7 +73,204 @@ namespace Mail_Sender_App
             }
             finally
             {
-                button1.Enabled = true;
+                send_btn.Enabled = true;
+
+            }
+
+        }
+        private void sendByGroup(MailMessage msg, SmtpClient smtp)
+        {
+            if (Emails.Text.Length == 0)
+            {
+                MessageBox.Show("Please Type the recipients");
+                return;
+            }
+            string[] emails = Emails.Text.Split(',');
+            msg.Body = "Dear " + name.Text + Environment.NewLine;
+            msg.Body += Body.Text;
+            msg.To.Clear();
+            foreach (string s in emails)
+            {
+                //TO-DO: optimize it to ignore invalid mails
+                msg.To.Add(s);
+            }
+            smtp.Send(msg);
+            MessageBox.Show("Mission Acomplished, Sent Email to " + msg.To.Count, "Result", MessageBoxButtons.OK);
+
+        }
+        private void sendByIndividual(/*MailMessage msg, SmtpClient smtp*/)
+        {
+            if (dataGridView1.RowCount == 0)
+            {
+                MessageBox.Show("Please open the data sheet that has the recipients");
+                return;
+            }
+            int cnt = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value == null || row.Cells[1].Value == null || row.Cells[0].Value.ToString() == "") continue;
+                string email = row.Cells[0].Value.ToString();
+                string name = row.Cells[1].Value.ToString();
+                string isSentBefore = row.Cells[2].Value.ToString();
+                if (isSentBefore == "Sent") continue;
+                // MessageBox.Show(email + "," + name);
+                msg.Body = "Dear " + name + "\n";
+                msg.Body += Body.Text;
+                cnt++;
+                msg.To.Clear();
+                msg.To.Add(email);
+
+                smtp.Send(msg);
+                row.Cells[2].Value = "Sent";
+              //  saveButton.PerformClick();
+            }
+            MessageBox.Show("Mission Acomplished, Sent Email to " + cnt, "Result", MessageBoxButtons.OK);
+            saveButton.PerformClick();
+            if (thread.IsAlive)
+                thread.Abort();
+
+        }
+
+        private void openSourceSheetbtn_Click(object sender, EventArgs e)
+        {
+            filePath = string.Empty;
+            string fileExt = string.Empty;
+            OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file  
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK) //if there is a file choosen by the user  
+            {
+                filePath = file.FileName; //get the path of the file  
+                fileExt = Path.GetExtension(filePath); //get the file extension  
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        DataTable dtExcel = new DataTable();
+                        dtExcel = ReadExcel(filePath, fileExt); //read excel file  
+                        dataGridView1.Visible = true;
+                        dataGridView1.DataSource = dtExcel;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error  
+                }
+            }
+        }
+
+        private DataTable ReadExcel(string fileName, string fileExt)
+        {
+            // install https://www.microsoft.com/en-us/download/confirmation.aspx?id=13255
+
+            string conn = string.Empty;
+            DataTable dtexcel = new DataTable();
+            if (fileExt.CompareTo(".xls") == 0)
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+            else
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=YES';"; //for above excel 2007  
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [Sheet1$]", con); //here we read data from sheet1  
+                    oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            return dtexcel;
+        }
+
+        private void dataGridView1_DataSourceChanged(object sender, EventArgs e)
+        {
+            RemoveEmptyColumns(ref dataGridView1);
+        }
+        public void RemoveEmptyColumns(ref DataGridView grdView)
+        {
+            int numOfcols = grdView.Columns.Count;
+            foreach (DataGridViewColumn clm in grdView.Columns)
+            {
+                //clm.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                if (clm.HeaderText.Length <= 3)//nead to think in a beter condition
+                {
+                    numOfcols--;
+                    grdView.Columns[clm.Index].Visible = false;
+                }
+            }
+            foreach (DataGridViewColumn clm in grdView.Columns)
+            {
+                clm.Width = (grdView.Width - 70) / numOfcols;
+            }
+            AdjustColumnSize(ref grdView, numOfcols);
+        }
+        public void AdjustColumnSize(ref DataGridView grdView, int numOfCols)
+        {
+            foreach (DataGridViewColumn clm in grdView.Columns)
+            {
+                clm.Width = (grdView.Width - 70) / numOfCols;
+            }
+            grdView.AutoResizeRows();
+        }
+        private void dataGridView1_SizeChanged(object sender, EventArgs e)
+        {
+            RemoveEmptyColumns(ref dataGridView1); //need to implemnt diffrent function than remove :no optimize performance
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            // creating Excel Application  
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            //File.Open(filePath, FileMode.OpenOrCreate);
+            // creating new WorkBook within Excel application  
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Open(filePath);
+            //Add(Type.Missing);
+            // creating new Excelsheet in workbook  
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            // see the excel sheet behind the program  
+            //app.Visible = true;
+            // get the reference of first sheet. By default its name is Sheet1.  
+            // store its reference to worksheet  
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+            // changing the name of active sheet  
+            // storing header part in Excel  
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            // storing Each row and column value to excel sheet  
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            // save the application  
+            workbook.Save();
+            // workbook.SaveAs(@"C:\Users\MahmoudHamdy-LAPTOP\OneDrive\Desktop\output111.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            // Exit from the application  
+            //workbook.Save();
+
+            app.Quit();
+
+        }
+
+        private void stopSendingBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thread.Interrupt();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
